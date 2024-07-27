608c0e0edb28ebecf7b7edcdae9ac245c59eb895
jhaemyre <jhaashish.ajha@gmail.com>
2024-07-27 22:44:40 +0530

    Bug fixed of logging and unexpected map behaviour

--- frontend/Quillsight/src/Hooks/useLogin.js
+++ frontend/Quillsight/src/Hooks/useLogin.js
@@ -3,6 +3,7 @@ import { useState } from 'react';
 import axios from 'axios';
 import useUserStore from '../Zustand/userInfoStore.js';
 import { useNavigate } from 'react-router-dom';
+import useBlogStore from '../Zustand/userBlogs.js';
 
 const useLogin = () => {
     const [isLoading, setIsLoading] = useState(false);
@@ -10,6 +11,10 @@ const useLogin = () => {
     const loggedUser = useUserStore((state) => state.loggedUser);
     const authStatus = useUserStore((state) => state.authStatus);
     const navigate = useNavigate()
+    const user = useUserStore((state) => state.userData);
+    const createdBlogStore = useBlogStore()
+    const createdBlogs = useBlogStore((state)=>state.createdBlogs)
+    const savedBlogs = useBlogStore((state)=>state.savedBlogs)
 
   const login = async (email, password) => {
     setIsLoading(true);
@@ -17,6 +22,24 @@ const useLogin = () => {
 
     try {
       const response = await axios.post('/api/v1/user/loginUser', { email, password });
+      console.log("sending request to backend for getting all updates ")
+        const getCreatedBlog = await axios.get('/api/v1/post/getyourpost')
+        const getSavedBlog = await axios.get("/api/v1/post/savedpost")
+        console.log("updates",getCreatedBlog);
+        console.log("getCreatedBlog",getCreatedBlog.data.data)
+        console.log("getSavedBlog",getSavedBlog);
+        console.log("getSavedBlog",getSavedBlog.data.data)
+        if (getCreatedBlog.data.error){
+          console.log("error in getting updates",getCreatedBlog.data.error);
+          throw new Error(getCreatedBlog.data.error)
+        }
+        if (getSavedBlog.data.error){
+          console.log("error in getting updates",getSavedBlog.data.error);
+          throw new Error(getSavedBlog.data.error)
+        }
+        createdBlogStore.getCreatedUpdate(getCreatedBlog.data.data)
+        createdBlogStore.getSavedUpdate(getSavedBlog.data.data.savedItems)
+
       //console.log("touching created")
       //const created = await axios.get('/api/v1/post/getyourpost')
       //console.log("created done")
