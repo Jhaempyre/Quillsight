109f200582cd42206a98fcddef730d754c8cac96
Bhavya Singh <bhavyasingh2602@gmail.com>
2024-07-29 11:55:24 +0530

    Bug fixed

--- frontend/Quillsight/src/Components/Preview.jsx
+++ frontend/Quillsight/src/Components/Preview.jsx
@@ -6,7 +6,7 @@ export default function Preview({ selectedTopic, posts }) {
       <h1 className="text-3xl font-bold text-gray-800 mb-8 text-center">{selectedTopic}</h1>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 flex-grow overflow-y-auto">
         {posts.map((post) => (
-          <div key={post._id} className="bg-white rounded-xl shadow-md overflow-hidden transition-all duration-300 hover:-translate-y-1 hover:shadow-lg">
+          <div key={post._id} className="bg-white rounded-xl shadow-md overflow-hidden transition-all duration-300 hover:-translate-y-1 hover:shadow-lg" style={{height:"300px"}}>
             <img src={post.image} alt={post.tittle} className="w-full h-48 object-cover" />
             <div className="p-6">
               <h2 className="text-xl font-semibold text-gray-800 mb-2">{post.tittle}</h2>
