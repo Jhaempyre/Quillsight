3664007aa2458267f19b7c078081dbf427ad68d3
Aashish Jha <jhaashish.ajha@gmail.com>
2024-07-29 10:36:03 +0530

    TOPICS Changed

--- frontend/Quillsight/src/Pages/DashBoard.jsx
+++ frontend/Quillsight/src/Pages/DashBoard.jsx
@@ -3,7 +3,29 @@ import Category from '../Components/Category.jsx'
 import Preview from '../Components/Preview.jsx'
 
 function DashBoard() {
-  const topics = ["Technology", "Science", "Art", "Music", "Sports", "Food", "Travel", "Fashion", "Health", "Education","ram","sita","gita","heera","mori","moti"];
+  const topics = [
+    'Technology',
+    'Travel',
+    'Health and Fitness',
+    'Food and Cooking',
+    'Fashion',
+    'Personal Development',
+    'Parenting',
+    'Finance',
+    'DIY and Crafts',
+    'Lifestyle',
+    'Business and Entrepreneurship',
+    'Politics and Current Affairs',
+    'Education',
+    'Fitness',
+    'Beauty',
+    'Home and Garden',
+    'Sports',
+    'Entertainment',
+    'love',
+    'Environment and Sustainability',
+    'Art and Culture'
+  ];
   const [selectedTopic, setSelectedTopic] = useState(topics[0]);
 
   const handleTopicSelect = (topic) => {
