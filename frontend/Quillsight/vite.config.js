2942d498ad44326d0ab5d4ca49fe953899e3c78e
Aashish Jha <jhaashish.ajha@gmail.com>
2024-07-29 21:48:36 +0530

    added vercel configuration and maintained proxy in this

--- frontend/Quillsight/vite.config.js
+++ frontend/Quillsight/vite.config.js
@@ -2,14 +2,9 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:8000',  //for development server i guess other iption are creating problems 
-        //other options of fallback will be written after mvp .
-
-      },
-    },
-  },
   plugins: [react()],
+  build: {
+    outDir: 'dist'
+  },
+  // No server proxy needed for production
 });
